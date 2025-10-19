c4472f4343c9fe9e4b11e31dfc1149e7abca3009
Jahiem Jarvis <wherejah@icloud.com>
2025-10-19 18:33:23 -0400

    Add full app files

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Start Icecast
+# Start Icecast with privilege drop
 icecast2 -c ./icecast.xml &
 sleep 2
 
-# Start FFmpeg to pull stream and write to wkmglive.mp3
+# Start FFmpeg to pull stream and write to file
 ffmpeg -re -i http://208.89.99.124:5004/auto/v6.1 \
--map 0:1 -acodec libmp3lame -ar 44100 -b:a 192k \
+-map 0:a -acodec libmp3lame -ar 44100 -b:a 192k \
 -f mp3 ./wkmglive.mp3 &
 
 # Wait for FFmpeg to start writing
 sleep 2
 
-# Start Ices2 to stream wkmglive.mp3 to Icecast
+# Start Ices2 to stream MP3 file to Icecast
 ices2 ./ices-playback.xml &
 
 # Start Node backend
